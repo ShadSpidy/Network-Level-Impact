5c1d1ed59f1540307ef450f2c0347243ea193073
ShadSpidy <shadmanakhter78@gmail.com>
2024-06-20 01:16:28 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st
+
+st.title("Network Level Impact Calculator")
+st.subheader("By: Shadman")
+
+# Getting user inputs
+total_traffic = st.number_input("Enter the Total Traffic:", min_value=0.0, step=0.1)
+impacted_node_traffic = st.number_input("Enter the Traffic catered by impacted nodes:", min_value=0.0, step=0.1)
+percentage_impact = st.number_input("Enter the current percentage impact (%):", min_value=0.0, max_value=100.0, step=0.1)
+
+# Calculation
+if st.button("Calculate"):
+    NW_level_impact = ((impacted_node_traffic / total_traffic) * 100) * (percentage_impact / 100)
+    st.write(f"Current Network Level Impact is {NW_level_impact:.2f}")
